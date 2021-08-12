2d7c7746792d2e099d74251fcfdd94aa5ee27a02
penggrace <penggrace96@gmail.com>
2021-08-12 12:44:34 -0400

    added models

--- server/app.js
+++ server/app.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const layout = require('../views/layout');
+const { db, Page, User } = require('../models');
 
 app.use(morgan('dev'));
 app.use(express.static('./public'));
 app.use(express.json());
 app.use(express.urlencoded({}));
-const layout = require('../views/layout');
 
 app.get('/',(req, res, next) => {
   res.send(layout('This is the main page!'));
 });
 
-app.listen(8000, () => {
-  console.log('Listening on port 8000');
-});
+const init = async () => {
+  await db.sync({force: true});
+
+  const PORT = 8000
+
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}!`);
+  });
+}
+
+init();
+
+
+db.authenticate()
+  .then(() => {
+    console.log('connected to the database');
+  })
